8b1a6a6a350f409fa5f6a51353c1d7e72e358341
Brett Wilkins <brett@brett.geek.nz>
2014-07-22 11:02:31 +1000

    Adding DescribeApps AWS call

---
+++ opsworks/describe_apps.go
@@ -0,0 +1,65 @@
+package opsworks
+
+import "bytes"
+import "encoding/json"
+import "github.com/bwilkins/aws"
+
+type DescribeAppsResponse struct {
+  Apps []App
+}
+
+type DescribeAppsRequest struct {
+  StackId string `json:",omitempty"`
+  AppIds []string `json:",omitempty"`
+}
+
+type App struct {
+  AppId string
+  AppSource AppSourceBlock
+  Attributes AttributesBlock
+  CreatedAt string
+  DataSources []DataSourceBlock
+  Description string
+  Domains []string
+  EnableSsl string
+  Name string
+  Shortname string
+  StackId string
+  Type string
+  SslConfiguration SslConfigurationBlock
+}
+
+type AppSourceBlock struct {
+  Password string
+  Revision string
+  SshKey string
+  Type string
+  Url string
+  Username string
+}
+
+type AttributesBlock map[string]string
+
+type DataSourceBlock struct {
+  Arn string
+  DatabaseName string
+  Type string
+}
+
+type SslConfigurationBlock struct {
+  Certificate string
+  Chain string
+  PrivateKey string
+}
+
+func DescribeApps(request DescribeAppsRequest) (*DescribeAppsResponse, error) {
+  bodyEncoded, err := json.Marshal(request)
+  if err != nil {
+    return nil, err
+  }
+
+  r, _ := aws.NewRequest("POST", "DescribeApps", EndpointDefinition, bytes.NewReader(bodyEncoded))
+
+  v := new(DescribeAppsResponse)
+  return v, r.Do(v)
+}
